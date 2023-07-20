b44b7e413066a1fca6b00ec4332d8db012c94010
Phisto <simon.cay.gaus@gmail.com>
2023-07-20 21:55:17 +0200

    Cleanup

--- server/database/mysql.go
+++ server/database/mysql.go
@@ -6,21 +6,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func Select(db *sql.DB, table string, objectIDs []int) (*sql.Rows, error) {
-
-	var query string
-	var vars []int
-	if len(objectIDs) == 0 {
-		query = "SELECT * FROM " + table + "s;"
-		vars = []int{}
-	} else {
-		placeholder := DBPlaceholderForIDs(objectIDs)
-		query = "SELECT * FROM " + table + "s WHERE " + table + "_id IN (" + placeholder + ");"
-		vars = objectIDs
-	}
-	return ExecuteRowQuery(db, query, InterfaceInt(vars))
-}
-
 func ExecuteRowQuery(db *sql.DB, query string, args []interface{}) (*sql.Rows, error) {
 
 	rows, err := db.Query(query, args...)
@@ -31,21 +16,4 @@ func ExecuteRowQuery(db *sql.DB, query string, args []interface{}) (*sql.Rows, e
 		return nil, err
 	}
 	return rows, nil
-}
-
-func ExecuteQuery(db *sql.DB, query string, args []interface{}) (sql.Result, error) {
-
-	stmt, err := db.Prepare(query)
-	if err != nil {
-		return nil, err
-	}
-	result, err := stmt.Exec(args...)
-	if err != nil {
-		return nil, err
-	}
-	err = stmt.Close()
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
 }
