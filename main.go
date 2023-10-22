11c85b2b96c860332953d9b264a8b18263f7e6d6
Phisto <simon.cay.gaus@gmail.com>
2023-10-23 01:50:39 +0200

    Send client cert

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func sendHeartbeat(conf *config.Config) {
 		timer := time.After(time.Second * 2)
 		<-timer
 		var beat *heartbeat.Heartbeat = &heartbeat.Heartbeat{Service: "festivals-identity-server", Host: "https://" + conf.ServiceBindHost, Port: conf.ServicePort, Available: true}
-		err := heartbeat.SendHeartbeat(conf.LoversEar, conf.ServiceKey, beat)
+		err := heartbeat.SendHeartbeat(conf.LoversEar, conf.ServiceKey, conf.TLSCert, conf.TLSKey, conf.TLSRootCert, beat)
 		if err != nil {
 			log.Error().Err(err).Msg("Failed to send heartbeat")
 		}
